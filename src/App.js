d1627107fd3a4fbc737aa4dd46c5d4c70eef57c3
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-11-19 23:15:11 +0600

    Done achivment part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import OurAchievements from './Componants/OutAchievements/OurAchievements';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome to Athena</h1>
+    <div>
+     <OurAchievements></OurAchievements>
     </div>
   );
 }
