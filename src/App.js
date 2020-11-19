06aedf5e554b1f9f72af2616c351e4607ff29244
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-11-19 23:26:36 +0600

    create choose team

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import OurAchievements from './Componants/OutAchievements/OurAchievements';
+import ChooseTeam from './Componants/ChooseTeam/ChooseTeam';
 
 function App() {
   return (
     <div>
      <OurAchievements></OurAchievements>
+     <ChooseTeam></ChooseTeam>
     </div>
   );
 }
---
+++ src/Componants/ChooseTeam/ChooseTeam.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChooseTeam.css';
+
+const ChooseTeam = () => {
+    return (
+        <div className = "container">
+            <h1>ChooseTeam</h1>
+        </div>
+    );
+};
+
+export default ChooseTeam;
