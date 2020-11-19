f54a10010e573a6ece7c98b1eaaff4fe532ff6c0
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-11-19 16:14:46 +0600

    creat OurAchievements componants

---
+++ src/Componants/OutAchievements/OurAchievements.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurAchievements = () => {
+    return (
+        <div>
+            <h1>Our Achievements</h1>
+        </div>
+    );
+};
+
+export default OurAchievements;
