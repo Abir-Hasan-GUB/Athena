3532d3da61cd09efc837f2d0c81a71749b028a41
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-11-20 12:39:39 +0600

    Alhamdulillah  All conflict resolved with done full project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ChooseTeam from './Componants/ChooseTeam/ChooseTeam';
+import Footer from './Componants/Footer/Footer';
+import OurAchievements from './Componants/OutAchievements/OurAchievements';
+import ContactUs from './Components/ContactUs/ContactUs';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+
+const App = () => {
+  return (
+    <div className = "container">
+      <Navbar></Navbar>
+      <Header></Header>
+      <ContactUs></ContactUs>
+      <OurAchievements></OurAchievements>
+      <ChooseTeam></ChooseTeam>
+      <Footer></Footer>
     </div>
   );
-}
+};
 
 export default App;
