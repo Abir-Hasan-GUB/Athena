6a03764e49b2122df74b85e9ab2445496fe56df5
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-11-20 14:04:12 +0600

    Sarvice part missing resolved

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import OurAchievements from './Componants/OutAchievements/OurAchievements';
 import ContactUs from './Components/ContactUs/ContactUs';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
+import Services from './Components/Services/Services';
 
 const App = () => {
   return (
     <div className = "container">
       <Navbar></Navbar>
       <Header></Header>
+      <Services></Services>
       <ContactUs></ContactUs>
       <OurAchievements></OurAchievements>
       <ChooseTeam></ChooseTeam>
